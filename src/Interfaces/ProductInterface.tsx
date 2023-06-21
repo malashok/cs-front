712e915685cb1163ee0ac146ef167b5fece7ce7d
natal <nataliia.malashok@ukma.edu.ua>
2023-06-22 00:47:17 +0300

    delete and edit product forms

---
+++ src/Interfaces/ProductInterface.tsx
@@ -0,0 +1,10 @@
+interface ProductInterface {
+    id: number;
+    name: string;
+    price: number;
+    amount: number;
+    group_name: string;
+    producer: string;
+    description: string;
+}
+export default ProductInterface;
