64365d85b57e2f880d988689e22c762ae6ba9697
anastasiia1210 <anastasiia.lysa@ukma.edu.ua>
2023-06-21 16:36:03 +0300

    create login

--- src/loginForm/LoginForm.tsx
+++ src/loginForm/LoginForm.tsx
@@ -3,10 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import "./form.css";
 const LoginForm = (props) => {
     const [formData, setFormData] = useState({
-        login: '',
-        password: '',
+        name: '',
+        password: ''
     });
-    const navigate = useNavigate();
+   const navigate = useNavigate();
     const changePage = () => {
         navigate('/products');
     };
@@ -16,50 +16,47 @@ const LoginForm = (props) => {
         // Perform registration logic with formData
         console.log(formData);
         // // Reset form fields
-        // fetch('http://localhost:5001/auth', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(formData)
-        // })
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         console.log('Response from server:', data);
-        //         changePage();
-        //         props.setAuthUser(true);
-        //         setFormData((prevFormData) => ({
-        //             ...prevFormData}));
-        //         props.setTrigger(false)
-        //         setFormData({
-        //             login: '',
-        //             password: '',
-        //         });
-        //     })
-        //     .catch((error) => {
-        //         alert('Login or password is INCORRECT')
-        //         console.error('Error:', error);
-        //     });
-
-
+        fetch('http://localhost:5001/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Response from server:', data);
+                changePage();
+                props.setAuthUser(true);
+                setFormData((prevFormData) => ({
+                    ...prevFormData}));
+                setFormData({
+                    name: "",
+                    password: ""
+                });
+            })
+            .catch((error) => {
+                alert('Логін або пароль є неправильним!')
+                console.error('Error:', error);
+            });
 
     };
 
-    return  (
+    return (!props.authUser) ? (
         <div className="login-page">
             <div className="form">
                 <form className="login-form" onSubmit={handleSubmit}>
                     <div className="forClose">
                     </div>
-                    <input type="text" placeholder="Логін" required value={formData.login}
-                           onChange={(e) => setFormData({ ...formData, login: e.target.value })} />
+                    <input type="text" placeholder="Логін" required value={formData.name}
+                           onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
                     <input type="password" placeholder="Пароль" required value={formData.password}
                            onChange={(e) => setFormData({ ...formData, password: e.target.value })}/>
                     <button type="submit" className="okButton">Ввійти</button>
                 </form>
             </div>
         </div>
-    );
-};
+    ):"";
+}
 
 export default LoginForm;
